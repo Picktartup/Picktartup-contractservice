ca4ba36bf8922b9b5491fc5f34f7201050227153
yyyeun <yummytomato@naver.com>
2024-11-20 18:40:45 +0900

    🐛 fix: ERD 변경에 따른 Entity, Mock 수정

---
+++ src/main/java/com/picktartup/contractservice/mock/StartupDetailsMock.java
@@ -0,0 +1,26 @@
+package com.picktartup.contractservice.mock;
+
+import com.picktartup.contractservice.entity.StartupDetails;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+
+public class StartupDetailsMock {
+    public static StartupDetails createMockStartupDetails() {
+
+        return StartupDetails.builder()
+                .startupId(1L)
+                .description("목업스타트업입니달라.")
+                .investmentStatus("비상장")
+                .investmentRound("Seed")
+                .address("서울 마포구 상암")
+                .ceoName("김목업")
+                .registrationNum("123-45-67890")
+                .contractPeriod(12)
+                .page("https://www.naver.com/")
+                .establishmentDate("2020.10.1")
+                .expectedRoi(150.0)
+                .roi(null)
+                .build();
+    }
+}
